633378178aaa62783a3bacba4f0170ca752498ba
Jace1111 <jose.a.acevedo01@utrv.edu>
2020-11-16 13:51:22 -0600

    1st commit sign up is not working

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,11 @@
+class DashboardController < ApplicationController
+    def index
+    end
+  
+    def clients
+    end
+  
+    def reports
+    end
+  end
+  
--- db/migrate/20201107024838_create_patients.rb
+++ db/migrate/20201107024838_create_patients.rb
@@ -2,12 +2,13 @@ class CreatePatients < ActiveRecord::Migration[6.0]
   def change
     create_table :Patients do |t|
       t.references :account
-      t.string :Name
-      t.string :Address
-      t.string :Phone_Number
-      t.string :Photo
+          t.string :Name
+          t.string :Address
+          t.string :Phone_Number
+          t.string :Photo
+          t.string :ypup[]
 
-      t.timestamps
+          t.timestamps
     end
   end
 end
